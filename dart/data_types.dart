0e2fd586282b3621b1ebea0ed0f7ef5a2bd2df49
ogutuderick <ogutuderick4@gmail.com>
2024-02-29 13:24:02 +0300

    commit all

---
+++ dart/data_types.dart
@@ -0,0 +1,17 @@
+//data types in data
+void main(){
+  int num1 = 75;      //integer datatype
+  double num2 = 75.50;     //double datatype
+  String myName = "Derick Ogutu";    //string datatype
+  List<String> countries =  ["Kenya", "Uganda", "Tanzania"];    //list datatype
+  Map<String, int> marks = {'Derick': 80, 'Nancy': 88, 'Aaron': 90};
+  var sum = num1 + num2;
+
+  //output
+  print("num1 is $num1");
+  print("nmu2 is $num2");
+  print("sum is $sum");
+  print("the string is $myName");
+  print("my list is $countries");
+  print("the maps are $marks");
+}
